5970ce678b69e07eb2cc0dd32412d702bbaea748
DiogoArcenioo <diogoleonardoa@gmail.com>
2026-05-19 09:55:45 -0300

    Correção chamada back

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    const backendUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: `${backendUrl}/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
